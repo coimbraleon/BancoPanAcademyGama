27cb47594e7e56c8401112e673bfe0d31e96ab8b
coimbraleon <coimbraleonardox@gmail.com>
2021-10-09 14:50:36 -0300

    Commitfirst

---
+++ PrimeiroProjeto/src/TipoVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TipoVariaveis {
+
+	public static void main(String[] args) {
+		byte tipoByte= 123;
+		short tipoShort= 12345;
+		char tipoChar= 'A';
+		float tipoFloat= 2.1f;
+		double tipoDouble= 1.52;
+		int tipoInt= 5789421;
+		long tipoLong= 1985431857l;
+		boolean tipoBooleano= true;
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " + tipoFloat);
+		System.out.println("Valor do tipoDouble = " + tipoDouble);
+		System.out.println("Valor do tipoInt = " + tipoInt);
+		System.out.println("Valor do tipoLong = " + tipoLong);
+		System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+	}
+}
